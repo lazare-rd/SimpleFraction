de78026fa0154c4d7e58b9aaaf8c0e9eb7307176
Lazare Ricour-Dumas <lazare.rd@gmail.com>
2022-09-30 14:56:32 +0200

    init constructors + update toString

--- Fraction.java
+++ Fraction.java
@@ -1,6 +1,31 @@
 class Fraction {
-    
-    public String toString() {
-        return "je suis une faction";
+    private final int DENOMINATOR;
+    private final int NUMERATOR; 
+
+    public Fraction(int NUMERATOR, int DENOMINATOR){
+        this.DENOMINATOR = DENOMINATOR ;
+        this.NUMERATOR = NUMERATOR ;
+    }
+
+    public Fraction(int NUMERATOR){
+        this.NUMERATOR = NUMERATOR ;
+        this.DENOMINATOR = 1;
+    }
+
+    public Fraction(){
+        this.DENOMINATOR = 1;
+        this.NUMERATOR = 0;
+    }
+
+    public String toString(){
+        return this.NUMERATOR + "/" + this.DENOMINATOR ;
+    }
+
+    public int getNumerator(){
+        return this.NUMERATOR;
+    }
+
+    public int getDenominator(){
+        return this.DENOMINATOR;
     }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
 class Main {
     public static void main(String[] args) {
-        Fraction fraction = new Fraction() ;
-        assert fraction.toString() == "je suis une faction";
+        Fraction fraction_nulle = new Fraction() ;
+        assert fraction_nulle.toString() == "0/1";
+
+        Fraction fraction_non_nulle = new Fraction(1, 2);
+        assert fraction_non_nulle.toString() == "1/2" ; 
+
+        Fraction fraction_entiere = new Fraction(2);
+        assert fraction_entiere.toString() == "2/1";
+        
         System.out.println("ça compile");
     }
 }
